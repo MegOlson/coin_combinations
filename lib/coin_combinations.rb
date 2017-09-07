8326f765966f7dc24a93bc6fcd170120d0983dde
Kristen Kulha and Megan Olson <megan@>
2017-09-07 08:47:46 -0700

    Returns number value in nickels and pennies

--- lib/coin_combinations.rb
+++ lib/coin_combinations.rb
@@ -1,6 +1,20 @@
+require 'pry'
+
 class Integer
   def coin_combinations
-    return self.to_s + " pennies"
+    number_input = self
+    coins = ''
+    until (number_input  < 1)
+      if(number_input >= 5)
+        nickel_number = number_input / 5
+        coins += nickel_number.to_s + " nickels "
+        number_input -= nickel_number * 5
+      else
+        coins += number_input.to_s + " pennies"
+        number_input -= number_input
+      end
+    end
+    coins
   end
 
 end
--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -3,6 +3,10 @@ require 'coin_combinations'
 
 describe("#coin_combinations") do
   it("returns number value in pennies") do
-    expect(8.coin_combinations).to(eq("8 pennies"))
+    expect(4.coin_combinations).to(eq("4 pennies"))
+  end
+
+  it("returns number value in nickels and pennies") do
+    expect(9.coin_combinations).to(eq("1 nickels 4 pennies"))
   end
 end
